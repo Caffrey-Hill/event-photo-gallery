f7f198b02b74bc8d11140add94e374ee87ef2e09
Jeff Caffrey-Hill <5513084+ReverentEngineer@users.noreply.github.com>
2020-05-04 14:27:10 -0400

    Clean-up admin endpoint.

--- event_photo_gallery/admin.py
+++ event_photo_gallery/admin.py
@@ -1,6 +1,6 @@
 from flask import request, abort, render_template, redirect, url_for, current_app, Blueprint, flash
 from flask_login import current_user
-from .models import User, Category
+from .models import User, Category, Photo, Vote
 from .utils import generate_passcode
 from . import db
 
@@ -13,25 +13,52 @@ def check_auth():
     elif not current_user.admin:
         abort(403)
 
-@admin.route('/', methods=['GET', 'POST'])
+@admin.route('/')
 def index():
+    stats = {
+        'users': User.query.count(),
+        'categories': Category.query.count(),
+        'votes': Vote.query.count(),
+        'photos': Photo.query.count(),
+    }
+    return render_template('pages/admin/index.html', **stats)
+
+@admin.route('/users', methods=['GET', 'POST'])
+def users():
     if request.method == "POST":
-        if request.form["action"] == "add_category":
-            name = request.form["name"]
-            category = Category(name=name)
-            db.session.add(category)
-            db.session.commit()
-        elif request.form["action"] == "add_user":
-            name = request.form["name"]
-            passcode = generate_passcode()
-            user = User(name=name, passcode=passcode)
+        if request.form["action"] == "add":
+            user = User(name=request.form["name"], passcode=generate_passcode())
             db.session.add(user)
             db.session.commit()
-        elif request.form["action"] == "delete_user":
+        elif (request.form["action"] == "delete" and 
+                current_user.id != request.form["id"]):
             user = User.query.filter_by(id=request.form["id"]).first()
             if user:
                 db.session.delete(user)
                 db.session.commit()
+            else:
+                abort(400)
+        else:
+            abort(400)
     users = User.query.all()
-    categories = Category.query.all() 
-    return render_template('pages/admin.html', users=users, categories=categories)
+    return render_template('pages/admin/users.html', users=users)
+
+@admin.route('/categories', methods=['GET', 'POST'])
+def categories():
+    if request.method == "POST":
+        if request.form["action"] == "add":
+            category = Category(name=request.form["name"])
+            db.session.add(category)
+            db.session.commit()
+        elif request.form["action"] == "delete":
+            category = Category.query.filter_by(id=request.form["id"]).first()
+            if category:
+                db.session.delete(category)
+                db.session.commit()
+            else:
+                abort(400)
+        else:
+            abort(400)
+
+    categories = Category.query.all()
+    return render_template('pages/admin/categories.html', categories=categories)
