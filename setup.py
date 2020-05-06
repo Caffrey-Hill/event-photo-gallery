3509286c40fe2d0129c0049064e94719517393a1
Jeff Caffrey-Hill <5513084+ReverentEngineer@users.noreply.github.com>
2020-05-06 11:22:49 -0400

    Pull in template and static files.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
+import os
+
+def get_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+             paths.append(os.path.join('..', path, filename))
+    return paths
+
+static_files = get_files('event_photo_gallery/static')
+template_files = get_files('event_photo_gallery/templates')
 
 setup(
     name="event-photo-gallery",
-    version='1.0.0',
+    version='1.0.1',
     platforms="all",
     packages=find_packages(),
     package_data={
-        "": ["*.html", "*.css"]
+        "": static_files + template_files
     },
     author="Jeff Caffrey-Hill",
     author_email="jeff@reverentengineer.com",
