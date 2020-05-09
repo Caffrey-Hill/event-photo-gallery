5b095621bfbd7ec9863abe207bd46f494e1f7a7a
Jeff Caffrey-Hill <5513084+ReverentEngineer@users.noreply.github.com>
2020-05-08 20:54:07 -0400

    Add dev_mode script and config.

---
+++ dev_mode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export FLASK_ENV=development
+export FLASK_APP=event_photo_gallery
+export EVENT_PHOTO_GALLERY_SETTINGS=../dev.cfg
+
+trap cleanup INT
+
+cleanup() {
+  rm -f ./dev.db
+}
+
+flask run
