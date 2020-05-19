5a792c3a58d091f3d919fae22cca470fec7e73f7
Jeff Caffrey-Hill <5513084+ReverentEngineer@users.noreply.github.com>
2020-05-19 11:03:54 -0400

    Fix default UPLOADS_FOLDER.

--- event_photo_gallery/default_settings.py
+++ event_photo_gallery/default_settings.py
@@ -3,7 +3,7 @@ import os
 SITE_TITLE = 'Event Photo Gallery'
 SECRET_KEY = os.urandom(16)
 ALLOWED_PHOTO_EXTENSIONS = ['gif','jpg','jpeg','png','svg']
-UPLOADS_FOLDER = 'uploads'
+UPLOADS_FOLDER = "/".join([os.path.dirname(__file__), 'uploads'])
 DISABLE_COMMENTS = False
 DISABLE_VOTES = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
