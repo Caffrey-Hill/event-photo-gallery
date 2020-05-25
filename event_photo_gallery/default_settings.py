bc4596438eaf846f787b7ef206a4189fdfc7f072
Jeff Caffrey-Hill <5513084+ReverentEngineer@users.noreply.github.com>
2020-05-25 17:26:29 -0400

    Remove OBE defualt settings for toggling.

--- event_photo_gallery/default_settings.py
+++ event_photo_gallery/default_settings.py
@@ -4,6 +4,4 @@ SITE_TITLE = 'Event Photo Gallery'
 SECRET_KEY = os.urandom(16)
 ALLOWED_PHOTO_EXTENSIONS = ['gif','jpg','jpeg','png','svg']
 UPLOADS_FOLDER = "/".join([os.path.dirname(__file__), 'uploads'])
-DISABLE_COMMENTS = False
-DISABLE_VOTES = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
