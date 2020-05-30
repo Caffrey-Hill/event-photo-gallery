c6c50cd712104b666f5531c115537992db6967f3
Jeff Caffrey-Hill <5513084+ReverentEngineer@users.noreply.github.com>
2020-05-30 22:39:54 +0000

    Add cache and fix tests.

--- tests/data.sql
+++ tests/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO users (name, passcode, admin) VALUES ('test', 'mypasscode', 1);
+INSERT INTO categories (name, uploads_enabled, voting_enabled, comments_enabled) VALUES ('test', 1, 1, 1);
